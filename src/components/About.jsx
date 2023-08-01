fb62b4846d39fb3ec8c527e649ef034bb7df5f9d
A Prabath <avishka2k@gmail.com>
2023-08-01 17:47:14 +0000

    update with detals

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Basic } from "../util/User";
+
 const AboutUs = () => {
   return (
     <section id="about" className="section">
@@ -16,7 +18,7 @@ const AboutUs = () => {
             <h2 className="text-8 fw-400 mb-3">
               Hi, I'm{" "}
               <span className="fw-700 border-bottom border-3 border-primary">
-                Callum Smith
+                {Basic.full_name}
               </span>
             </h2>
             <p className="text-5">
@@ -34,7 +36,7 @@ const AboutUs = () => {
             <div className="featured-box style-4">
               <div className="featured-box-icon text-25 fw-500 bg-primary rounded-circle">
                 <span className="wow heartBeat" data-wow-delay="1.3s">
-                  22
+                  3+
                 </span>
               </div>
               <h3 className="text-7 wow rubberBand" data-wow-delay="2s">
@@ -46,23 +48,23 @@ const AboutUs = () => {
         <div className="row gy-3 mt-4">
           <div className="col-6 col-lg-3 wow fadeInUp">
             <p className="text-muted fw-500 mb-0">Name:</p>
-            <p className="text-4 text-dark fw-600 mb-0">Callum Smith</p>
+            <p className="text-4 text-dark fw-600 mb-0">{Basic.full_name}</p>
           </div>
           <div className="col-6 col-lg-3 wow fadeInUp" data-wow-delay="0.2s">
             <p className="text-muted fw-500 mb-0">Email:</p>
             <p className="text-4 fw-600 mb-0">
               <a className="link-dark" href="mailto:chat@callum.com">
-                chat@callum.com
+                {Basic.email}
               </a>
             </p>
           </div>
           <div className="col-6 col-lg-3 wow fadeInUp" data-wow-delay="0.3s">
             <p className="text-muted fw-500 mb-0">Date of birth:</p>
-            <p className="text-4 text-dark fw-600 mb-0">11 November, 1987</p>
+            <p className="text-4 text-dark fw-600 mb-0">{Basic.bDay}</p>
           </div>
           <div className="col-6 col-lg-3 wow fadeInUp" data-wow-delay="0.4s">
             <p className="text-muted fw-500 mb-0">From:</p>
-            <p className="text-4 text-dark fw-600 mb-0">Los Angeles, USA.</p>
+            <p className="text-4 text-dark fw-600 mb-0">{Basic.Address}</p>
           </div>
         </div>
       </div>
---
+++ src/util/User.jsx
@@ -0,0 +1,45 @@
+export const Basic = {
+  full_name: "Avishka Prabath",
+  fname: "Avishka",
+  lname: "Prabath",
+  email: "avishka2k@gmail.com",
+  bDay: "30 December 2000",
+  phone: "+94712716098",
+  Address: "Rathnapura, Sri Lanka",
+  myLocation:
+    "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d495.31575058114817!2d80.38448581595352!3d6.7054687042873296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2slk!4v1686588395799!5m2!1sen!2slk",
+  languages: "Sinhala, English",
+};
+
+export const Social = {
+  linedin: "https://www.linkedin.com/in/avishkap",
+  github: "https://github.com/avishka2k",
+  facebook: "https://www.facebook.com/avishka2k",
+};
+
+export const MySkills = [
+  {
+    name: "Web",
+    score: "80",
+  },
+  {
+    name: "Mobile App",
+    score: "70",
+  },
+  {
+    name: "Java",
+    score: "70",
+  },
+  {
+    name: "JavaScript",
+    score: "80",
+  },
+  {
+    name: "Jenkins",
+    score: "80",
+  },
+  {
+    name: "Ansible",
+    score: "60",
+  },
+];
